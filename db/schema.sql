327f06dc2de33df53ede2510a0000fcb97d665fb
finntendoverse <finnlambouris616@gmail.com>
2024-04-22 23:00:12 -0400

    added schema for tables

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+id INT AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+id INT AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(6, 2) NOT NULL,
+department_id INT,
+PRIMARY KEY(id),
+FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY(id),
+FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
+);
--- index.js
+++ index.js
@@ -1,30 +1,60 @@
-handlePrompting = function() {
-  let getLogoText = async function() {
-      let employeeManager = await inquirer.prompt(
-          [
+const inquirer = require("inquirer");
+
+let initializeApp = async function() {
+    let employeeManager = await inquirer.prompt(
+        [
             {
                 name: 'options',
                 type: 'list',
                 message: 'What would you like to do?',
                 choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
             }
-          ]
-      )
-  
-      if (employeeManager.options === "View All Departments") {
-        
-      } else if (employeeManager.options === "View All Roles") {
+        ]
+    )
 
-      } else if (employeeManager.options === "View All Employees") {
-
-      } else if (employeeManager.options === "Add a Department") {
-
-      } else if (employeeManager.options === "Add A Role") {
-
-      } else if (employeeManager.options === "Add An Employee") {
-
-      } else if (employeeManager.options === "Update An Employee Role") {
-        
-      }
-  }
+    if (employeeManager.options === "View All Departments") {
+        viewAllDepartments();
+    } else if (employeeManager.options === "View All Roles") {
+        viewAllRoles();
+    } else if (employeeManager.options === "View All Employees") {
+        viewAllEmployees();
+    } else if (employeeManager.options === "Add a Department") {
+        addADepartment();
+    } else if (employeeManager.options === "Add A Role") {
+        addARole();
+    } else if (employeeManager.options === "Add An Employee") {
+        addAnEmployee();
+    } else if (employeeManager.options === "Update An Employee Role") {
+        updateAnEmployeeRole();
+    }
 }
+
+let viewAllDepartments = function() {
+
+}
+
+let viewAllRoles = function() {
+    
+}
+
+let viewAllEmployees = function() {
+    
+}
+
+let addADepartment = function() {
+    
+}
+
+let addARole = function() {
+    
+}
+
+let addAnEmployee = function() {
+    
+}
+
+let updateAnEmployeeRole = function() {
+    
+}
+
+initializeApp();
