8cc39c21071c91fe1af977d91e07579fb92302a1
finntendoverse <finnlambouris616@gmail.com>
2024-04-25 00:19:46 -0400

    added manager picking to employee adding function

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const Table = require("cli-table3");
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'mySQLroot',
-      database: 'employee_tracker'
+      host: "localhost",
+      user: "root",
+      password: "mySQLroot",
+      database: "employee_tracker"
     },
     console.log(`Connected to the employee_tracker database.`)
   );
@@ -16,10 +16,10 @@ let initializeApp = async function() {
     let employeeManager = await inquirer.prompt(
         [
             {
-                name: 'options',
-                type: 'list',
-                message: 'What would you like to do?',
-                choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role']
+                name: "options",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add A Role", "Add An Employee", "Update An Employee Role"]
             }
         ]
     )
@@ -136,9 +136,9 @@ let addADepartment = async function() {
     let newDepartment = await inquirer.prompt(
         [
             {
-                name: 'department',
-                type: 'input',
-                message: 'What is the name of the new department?',
+                name: "department",
+                type: "input",
+                message: "What is the name of the new department?",
             }
         ]
     )
@@ -164,9 +164,9 @@ let addARole = async function() {
         let newRole = await inquirer.prompt(
             [
                 {
-                    name: 'role',
-                    type: 'input',
-                    message: 'What is the name of the new role?',
+                    name: "role",
+                    type: "input",
+                    message: "What is the name of the new role?",
                 }
             ]
         )
@@ -179,9 +179,9 @@ let addARole = async function() {
         let newSalary = await inquirer.prompt(
             [
                 {
-                    name: 'salary',
-                    type: 'input',
-                    message: 'What is the salary of the new role?',
+                    name: "salary",
+                    type: "input",
+                    message: "What is the salary of the new role?",
                 }
             ]
         )
@@ -202,17 +202,17 @@ let addARole = async function() {
         let department = await inquirer.prompt(
             [
                 {
-                    name: 'department',
-                    type: 'list',
-                    message: 'Which department does the new role belong to?',
+                    name: "department",
+                    type: "list",
+                    message: "Which department does the new role belong to?",
                     choices: departments,
                 }
             ]
         )
-        updateDatabase(newRole, newSalary, department)
+        updateDepartmentDatabase(newRole, newSalary, department)
     };
 
-    let updateDatabase = function(newRole, newSalary, department) {
+    let updateDepartmentDatabase = function(newRole, newSalary, department) {
         db.query("SELECT * FROM department", (err, res) => {
             console.log(res);
 
@@ -238,6 +238,128 @@ let addARole = async function() {
 
 let addAnEmployee = function() {
 
+    let addEmployeeFirstname = async function() {
+        let firstname = await inquirer.prompt(
+            [
+                {
+                    name: "firstname",
+                    type: "input",
+                    message: "What is the employee's first name?",
+                }
+            ]
+        )
+        addEmployeeLastname(firstname);
+    }
+
+    addEmployeeFirstname();
+
+    let addEmployeeLastname = async function(firstname) {
+        let lastname = await inquirer.prompt(
+            [
+                {
+                    name: "lastname",
+                    type: "input",
+                    message: "What is the employee's last name?",
+                }
+            ]
+        )
+        viewRoles(firstname, lastname);
+    }
+
+    let viewRoles = function(firstname, lastname) {
+        db.query("SELECT * FROM role", (err, res) => {
+            var roles = []
+            res.forEach(role => {
+                roles.push(role.title);
+            })
+            addEmployeeRole(firstname, lastname, roles);
+        });
+    }
+
+    let addEmployeeRole = async function(firstname, lastname, roles) {
+        let role = await inquirer.prompt(
+            [
+                {
+                    name: "role",
+                    type: "list",
+                    message: "What is the employee's role?",
+                    choices: roles,
+                }
+            ]
+        )
+        viewManagers(firstname, lastname, role);
+    }
+
+    let viewManagers = function(firstname, lastname, role) {
+        db.query(
+            `SELECT CONCAT(first_name, " ", last_name) AS manager FROM employee`, (err, res) => {
+                var managers = ["none"];
+                res.forEach(row => {
+                    managers.push(row.manager);
+                })
+                addEmployeeManager(firstname, lastname, role, managers);
+        });
+    }
+
+    let addEmployeeManager = async function(firstname, lastname, role, managers) {
+        let manager = await inquirer.prompt(
+            [
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "Who is the employee's manager?",
+                    choices: managers,
+                }
+            ]
+        )
+        updateEmployeeDatabase(firstname, lastname, role, manager);
+    }
+
+    let updateEmployeeDatabase = function(firstname, lastname, role, manager) {
+        let updateInfo = async function() {
+            db.query("SELECT * FROM role", (err, res) => {
+                
+                let selectedRole = null
+                res.forEach(row => {
+                    if(row.title === role.role) {
+                        selectedRole = row.id;
+                    }
+                });
+
+                db.query(
+                    `INSERT INTO employee (first_name, last_name, role_id)
+                    VALUES
+                        ("${firstname.firstname}", "${lastname.lastname}", "${selectedRole}")`, (err, res) => {
+                            console.log(res);
+                            updateManager(selectedRole);
+                        }
+                );
+            });
+        }
+        
+        updateInfo();
+
+        let updateManager = async function(selectedRole) {
+            db.query("SELECT * FROM employee", (err, res) => {
+
+                let selectedManager = null
+                res.forEach(row => {
+                    if(row.first_name + " " + row.last_name === manager.manager) {
+                        selectedManager = row.id;
+                    }
+                });
+
+                db.query(
+                    `UPDATE employee
+                    SET manager_id = ${selectedManager}
+                    WHERE first_name = "${firstname.firstname}" AND last_name = "${lastname.lastname}" AND role_id = "${selectedRole}"`, (req, res) => {
+                        console.log(res);
+                        initializeApp();
+                    }
+                )
+            });
+        }
+    }
 }
 
 let updateAnEmployeeRole = function() {
